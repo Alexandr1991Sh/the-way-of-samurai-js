1ce13cbf9cba83836e26361ca8076fa0a4ebb28e
Alexandr1991Sh <an1991sh@yandex.ru>
2023-02-28 18:22:15 +0300

    21 Урок. Верстаем страницу Диалогов)

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import s from './Dialogs.module.css'
 
-const Dialogs = ()=>{
-    return(
-      <div>
-          Dialogs
-      </div>
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Alexandr
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Slava
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
     )
 }
 
